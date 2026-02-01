aad52a01ac6d3f07b47deaecc8eb470b9ba3ded3
Luis Lopes <luismvlopes@gmail.com>
2026-02-01 00:16:06 +0000

    first commit

---
+++ src/test/java/com/luismvlopes/EntreAjuda/EntreajudaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luismvlopes.EntreAjuda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntreajudaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
